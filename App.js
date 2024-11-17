55f26dedb32ddf6cde7a16d2333366b7de75c268
i-umayr <ranaumair1326@gmail.com>
2024-11-18 01:56:39 +0500

    added restaurant-view feature

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
 import { SafeAreaView, StyleSheet, Text, View, StatusBar } from "react-native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { React } from "react";
-import { Searchbar } from "react-native-paper";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  list: {
-    padding: 16,
-    backgroundColor: "blue",
-    flex: 1,
-  },
-});
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,31 @@
+import { SafeAreaView, StyleSheet, Text, View, StatusBar } from "react-native";
+import { React } from "react";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+
+export const RestaurantsScreen = () => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Searchbar />
+    </View>
+    <View style={styles.list}>
+      <RestaurantInfoCard />
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  list: {
+    padding: 16,
+    backgroundColor: "blue",
+    flex: 1,
+  },
+});
