673a8baa111fb642f03429dfcd5ad3a7d5683b4e
i-umayr <ranaumair1326@gmail.com>
2024-11-21 02:41:01 +0500

    finished setting up mock data

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Text } from "react-native";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { Ionicons } from "@expo/vector-icons";
-import { restaurantsRequest } from "./src/services/restaurants/restaurants.service";
 
 const Settings = () => (
   <SafeArea>
