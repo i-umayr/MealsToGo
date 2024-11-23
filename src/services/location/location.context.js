5be73d5c93522290b2cc0883e8aabcde6ff1b05e
i-umayr <ranaumair1326@gmail.com>
2024-11-24 02:08:18 +0500

    cleaning imports

--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 import { locationRequest, locationTransform } from "./location.service";
 
 export const LocationContext = createContext();
